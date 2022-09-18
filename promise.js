e80d7367d910ec05c14141b768e76056fd08a9e4
hambo31 <hambo.jaya@gmail.com>
2022-09-18 11:10:20 +0700

    buat fungsi promiseOutput

--- promise.js
+++ promise.js
@@ -1,8 +1,34 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+//const promiseOutput = null;
+let kond = 0;
+function promiseOutput(param) {
+  promiseTheaterIXX().then(function (result) {
+    //console.log(result[0].hasil);
 
-module.exports = {
-  promiseOutput,
-};
+    result.forEach(function (a) {
+      if (param == "marah") {
+        a.hasil == param ? kond++ : kond;
+      } else a.hasil == param ? kond++ : kond;
+    });
+    //console.log(kond);
+  });
+
+  promiseTheaterVGC().then(function (result) {
+    //console.log(result[0].hasil);
+
+    result.forEach(function (a) {
+      if (param == "marah") {
+        a.hasil == param ? kond++ : kond;
+      } else a.hasil == param ? kond++ : kond;
+    });
+
+    console.log(kond);
+  });
+}
+
+promiseOutput("marah");
+// module.exports = {
+//   promiseOutput,
+// };
